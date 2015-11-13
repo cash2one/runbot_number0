48e244b4d3729c55bf882674ce71ca1538591f47
Pei Pei <peipei02431@163.com>
2015-11-13 15:44:17 +0800

    add qbo_camera-master package,another camera driver

---
+++ qbo_camera-master/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/opt/ros/groovy/stacks/ros_workspace/src/OpenQbo/qbo_camera-master/lib/libgtest.pdb"
+  "/opt/ros/groovy/stacks/ros_workspace/src/OpenQbo/qbo_camera-master/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
