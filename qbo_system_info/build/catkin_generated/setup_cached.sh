51f81edb14186d99784f17a33d379a8a003f5396
Pei Pei <peipei02431@163.com>
2015-11-13 15:45:39 +0800

    add qbo_system_info package

---
+++ qbo_system_info/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/runji/ros_workspace/src/OpenQbo/qbo_system_info:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
